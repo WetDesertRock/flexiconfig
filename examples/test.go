60399371257692ca2307db1388de3a8d3fbcc57f
WetDesertRock <wetdesertrock@gmail.com>
2019-09-20 07:22:32 -0600

    Clean  up test.go

--- examples/test.go
+++ examples/test.go
@@ -24,14 +24,7 @@ func main() {
 	settings := flexiconfig.NewSettings()
 	fmt.Println(settings.LoadJSONFile("./test.json"))
 	fmt.Println(settings.LoadLuaFile("./test2.lua"))
-	// 	fmt.Println(settings.LoadLuaString(`return {
-	// 		three = "333",
-	// 		seven = "7",
-	// 		five = {
-	// 			eight = "8"
-	// 		}
-	// 	}
-	// `))
+
 	settings.Print()
 	fmt.Println(settings.GetString("three", "nonono"))
 	fmt.Println(settings.GetFloat("four", -1))
